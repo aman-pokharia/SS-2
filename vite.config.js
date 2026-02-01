522230540df1d9eca9ac3c804b13b8ed6020a7b7
AMAN POKHARIA <pokhariaaman2@gmail.com>
2026-02-01 18:10:18 +0530

    Remove Tailwind CSS plugin from vite.config.js
    Removed Tailwind CSS plugin from Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  base:"/SS-2/",
+  base: '/SS-2/',
+  plugins: [react()],
 })
